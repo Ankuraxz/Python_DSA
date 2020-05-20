b59b8ec2122cea65b365bdbba82a99f1f6e62d2e
Ankur <34418414+Ankuraxz@users.noreply.github.com>
2020-05-20 23:21:54 +0530

    Create maxm no of given digit.py
    https://www.geeksforgeeks.org/given-an-array-of-numbers-arrange-the-numbers-to-form-the-biggest-number/

---
+++ Array/Array Rearrangement/maxm no of given digit.py
@@ -0,0 +1,36 @@
+arr = [1, 34, 3, 98, 9, 76, 45, 4]
+l = len(arr)
+digits =[]
+
+# # Uncomment and use either of the 2 digitconverter
+
+def digitconverter(num):
+     # digit = []
+     while num !=0:
+         digits.append(num%10)
+         num = num//10
+
+# def digitconverter(num):
+#     number = str(num)
+#     for i in range(len(number)):
+#         digits.append(int(number[i]))
+
+
+    
+
+for i in range(l):
+    if arr[i] >=10: # if number is greater than equal to 10, it is seperated in digits, and appended in Digits list
+        digitconverter(arr[i])
+    else:
+        digits.append(arr[i])
+        
+# print(digits)
+digits.sort() # Ascending order of all digits
+digits = digits[::-1] #descending order sort
+# print(digits) # array of max no. possible
+n = "".join(map(str,digits)) # converting every digit to str
+
+print(n) # required max no. possible
+
+
+
